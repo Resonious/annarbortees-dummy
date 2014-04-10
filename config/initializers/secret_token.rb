9c47e914418313ed76a9440137f0cc9224477d4e
Nigel Baillie <metreckk@gmail.com>
2014-04-10 10:49:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnnarborteesDummy::Application.config.secret_key_base = 'f4396b24e65801f071f8062fd6a5717b4bb31c4b0320aa7e530b7fc87c461e9e25d51017e760675727237be41eb14a1fa1a2a0d8c2c9da0721ad8db03ff1d111'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnnarborteesDummy::Application.config.session_store :cookie_store, key: '_annarbortees-dummy_session'
